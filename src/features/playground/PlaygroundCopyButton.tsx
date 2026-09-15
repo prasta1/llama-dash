db8b25c5fdc5ebf98b862950632be40afb910454
patrick ruster <155988469+prasta1@users.noreply.github.com>
2026-09-15 11:12:37 -0400

    Deleted PlaygroundActiveModelCell.tsx

--- src/features/playground/PlaygroundCopyButton.tsx
+++
@@ -1,9 +0,0 @@
-import { CopyButton } from '../../components/CopyButton'
-
-type Props = {
-  text: string
-}
-
-export function PlaygroundCopyButton({ text }: Props) {
-  return <CopyButton text={text} />
-}
